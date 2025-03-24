d11cb969de255841e7337d425b9c17ac83f34349
DomiHub <adomi8049@gmail.com>
2025-03-24 21:07:20 +0100

    Creación de Main_Page

---
+++ app/src/main/java/com/domingo/instantcars/coche.kt
@@ -0,0 +1,10 @@
+package com.domingo.instantcars
+
+data class Coche(
+    val marca: String = "",
+    val modelo: String = "",
+    val year: Int = 0,
+    val precio: Int = 0,
+    val imagen: String = "",
+    val km: Int = 0
+)
